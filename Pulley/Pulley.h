61af95b4ead462c82ec6f2d5680c7ab0c613a72b
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-01-31 16:54:00 +0100

    Added a Pulley framework to support Carthage

---
+++ Pulley/Pulley.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pulley.
+FOUNDATION_EXPORT double PulleyVersionNumber;
+
+//! Project version string for Pulley.
+FOUNDATION_EXPORT const unsigned char PulleyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pulley/PublicHeader.h>
+
+
