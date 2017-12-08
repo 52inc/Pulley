16739439c977d937a16354f40669ef028ecb0e24
Brendan Lee <brendan@52inc.com>
2017-12-08 17:25:23 -0500

    Fix Pulley's use of a now deprecated method. Bump podspec version.

--- PulleyLib/PulleyViewController.swift
+++ PulleyLib/PulleyViewController.swift
@@ -440,7 +440,7 @@ open class PulleyViewController: UIViewController, PulleyDrawerViewControllerDel
             
             if supportedPositions.contains(drawerPosition)
             {
-                setDrawerPosition(position: drawerPosition)
+                setDrawerPosition(position: drawerPosition, animated: true)
             }
             else
             {
