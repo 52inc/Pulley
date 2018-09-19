b23402786831d52f42eabe0a886123a46c26c83d
Harish Yerra <hyerra08@gmail.com>
2018-09-19 17:46:57 -0500

    Update for Xcode 10

--- Pulley/AppDelegate.swift
+++ Pulley/AppDelegate.swift
@@ -13,8 +13,8 @@ import Pulley
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool
     {
         // Override point for customization after application launch.
         
