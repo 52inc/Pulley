42d9a735ff862bd95b61420630c82c89f8756d98
ulmentflam <johnwowen3@gmail.com>
2019-03-31 15:56:21 -0400

    Fixing Xcode 10.2 compiler warning via issue #326

--- PulleyLib/UIViewController+PulleyViewController.swift
+++ PulleyLib/UIViewController+PulleyViewController.swift
@@ -9,7 +9,7 @@
 public extension UIViewController {
 
     /// If this viewController pertences to a PulleyViewController, return it.
-    public var pulleyViewController: PulleyViewController? {
+    var pulleyViewController: PulleyViewController? {
         var parentVC = parent
         while parentVC != nil {
             if let pulleyViewController = parentVC as? PulleyViewController {
